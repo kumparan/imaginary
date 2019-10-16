0bb8a087998ccb6354fcee95f8f88a92ab60c755
fajrifernanda <fajri.fernanda@kumparan.com>
2019-10-16 13:32:17 +0700

    add manipulate, for no width height

--- image.go
+++ image.go
@@ -100,6 +100,12 @@ func Resize(buf []byte, o ImageOptions) (Image, error) {
 	return Process(buf, opts)
 }
 
+func Manipulate(buf []byte, o ImageOptions) (Image, error) {
+	//for manipulating without width or height input
+	opts := BimgOptions(o)
+	return Process(buf, opts)
+}
+
 func Fit(buf []byte, o ImageOptions) (Image, error) {
 	if o.Width == 0 || o.Height == 0 {
 		return Image{}, NewError("Missing required params: height, width", BadRequest)
