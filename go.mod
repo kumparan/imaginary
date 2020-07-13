9a9527610e4f1c6a9bc084cd5b02d35719db56a6
fajrifernanda <fajri.fernanda@kumparan.com>
2020-07-13 15:58:30 +0700

    feature: handle resizing gif

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/aws/aws-sdk-go v1.25.40
 	github.com/banzaicloud/logrus-runtime-formatter v0.0.0-20180617171254-12df4a18567f
 	github.com/certifi/gocertifi v0.0.0-20200211180108-c7c1fbc02894 // indirect
+	github.com/discordapp/lilliput v0.0.0-20191204003513-dd93dff726a5
 	github.com/evalphobia/logrus_sentry v0.8.2
 	github.com/getsentry/raven-go v0.2.0 // indirect
 	github.com/go-redsync/redsync v1.1.1
--- image.go
+++ image.go
@@ -11,6 +11,8 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/discordapp/lilliput"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/kumparan/imaginary/config"
@@ -178,6 +180,12 @@ func Manipulate(buf []byte, o ImageOptions) (Image, error) {
 
 	//manipulating without width, height, aspecratio input
 	opts := BimgOptions(o)
+	opts.Embed = true
+
+	if o.IsDefinedField.NoCrop {
+		opts.Crop = !o.NoCrop
+	}
+
 	return Process(buf, opts)
 }
 
@@ -517,6 +525,10 @@ func Process(buf []byte, opts bimg.Options) (out Image, err error) {
 		}
 	}()
 
+	if opts.Type == bimg.GIF || bimg.DetermineImageType(buf) == bimg.GIF {
+		return handleGIFResizing(buf, opts)
+	}
+
 	buf, err = bimg.Resize(buf, opts)
 	if err != nil {
 		return Image{}, err
@@ -524,4 +536,72 @@ func Process(buf []byte, opts bimg.Options) (out Image, err error) {
 
 	mime := GetImageMimeType(bimg.DetermineImageType(buf))
 	return Image{Body: buf, Mime: mime}, nil
+}
+
+// handleGIFResizing handle gif and animated gif resizing using lilliput library
+func handleGIFResizing(buf []byte, opts bimg.Options) (out Image, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			switch value := r.(type) {
+			case error:
+				err = value
+			case string:
+				err = errors.New(value)
+			default:
+				err = errors.New("lilliput internal error")
+			}
+			out = Image{}
+		}
+	}()
+
+	// create new decoder
+	decoder, err := lilliput.NewDecoder(buf)
+
+	// this error reflects very basic checks,
+	// mostly just for the magic bytes of the file to match known image formats
+	if err != nil {
+		return Image{}, NewError(fmt.Sprintf("error decoding image, %s\n", err), InternalError)
+	}
+	defer decoder.Close()
+
+	header, err := decoder.Header()
+	// this error is much more comprehensive and reflects
+	// format errors
+	if err != nil {
+		return Image{}, NewError(fmt.Sprintf("error reading image header, %s\n", err), InternalError)
+	}
+
+	ops := lilliput.NewImageOps(10000)
+	defer ops.Close()
+
+	// create a buffer to store the output image, 50MB in this case
+	outputImg := make([]byte, 20*1024*1024)
+
+	resizeMethod := lilliput.ImageOpsNoResize
+	if opts.Crop {
+		fmt.Println("crop", opts.Crop)
+		resizeMethod = lilliput.ImageOpsFit
+	}
+
+	if opts.Width == header.Width() && opts.Height == header.Height() {
+		resizeMethod = lilliput.ImageOpsNoResize
+	}
+
+	// lilliput image opts
+	lilliputImageOpts := &lilliput.ImageOptions{
+		FileType:             ".gif",
+		Width:                opts.Width,
+		Height:               opts.Height,
+		ResizeMethod:         resizeMethod,
+		NormalizeOrientation: true,
+	}
+
+	// resize and transcode image
+	outputImg, err = ops.Transform(decoder, lilliputImageOpts, outputImg)
+	if err != nil {
+		return Image{}, NewError(fmt.Sprintf("error transforming image, %s\n", err), InternalError)
+	}
+
+	mime := GetImageMimeType(bimg.DetermineImageType(outputImg))
+	return Image{Body: outputImg, Mime: mime}, nil
 }
