39cb15b8888cfc44aa4ecd723964a5e008ad7615
Francisco <francisco.francisco@kumparan.com>
2024-04-26 16:52:56 +0700

    fix

--- log_test.go
+++ log_test.go
@@ -33,7 +33,8 @@ func TestLogInfo(t *testing.T) {
 
 	data := string(buf)
 	if strings.Contains(data, http.MethodGet) == false ||
-		strings.Contains(data, "HTTP/1.1") == false {
+		strings.Contains(data, "HTTP/1.1") == false ||
+		strings.Contains(data, "200") == false {
 		t.Fatalf("Invalid log output: %s", data)
 	}
 }
